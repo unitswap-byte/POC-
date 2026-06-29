fcd3ef21e67ca70d415322d0a9677d15b971243a
unitswap-byte <unitswap@gmail.com>
2026-06-29 19:53:43 +0800

    Update next.config.js

--- CZTI-PoC-Demo-V1/next.config.js
+++ CZTI-PoC-Demo-V1/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
-  typescript: { ignoreBuildErrors: false },
-  eslint: { ignoreDuringBuilds: true }
+    react StrictMode: true,
+    typescript: { ignoreBuildErrors: false },
+    eslint: { ignoreDuringBuilds: true }
 };
-module.exports = nextConfig;
