c00aefc614a4665a7c0a06f441a75716df9df1d7
unitswap-byte <unitswap@gmail.com>
2026-06-29 21:07:04 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  typescript: { ignoreBuildErrors: false },
-  eslint: { ignoreDuringBuilds: true }
+  reactStrictMode: true
 };
+
 module.exports = nextConfig;
