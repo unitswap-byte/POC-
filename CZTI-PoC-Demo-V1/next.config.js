3680277b89728adcbbc198850bcd9efa3c938d4e
unitswap-byte <unitswap@gmail.com>
2026-06-29 20:05:21 +0800

    Delete CZTI-PoC-Demo-V1 directory

--- CZTI-PoC-Demo-V1/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    react StrictMode: true,
-    typescript: { ignoreBuildErrors: false },
-    eslint: { ignoreDuringBuilds: true }
-};
